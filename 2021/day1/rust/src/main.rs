1188b3f2ead695a9d44d814cf6f0b338f9c6130c
Sam Schlegel <git@lutin.us>
2021-11-30 22:01:20 -0800

    add a comment

--- 2021/day1/rust/src/main.rs
+++ 2021/day1/rust/src/main.rs
@@ -9,6 +9,8 @@ fn count_increases_part2(input: &[i64]) -> usize {
     input
         .windows(3)
         .map(|w| w.iter().sum())
+        // We could use something more fancy to support this over arbitrary iterators but our data is small enough
+        // that just collecting is probably faster
         .collect::<Vec<i64>>()
         .windows(2)
         .map(|w| (w[1] > w[0]) as usize)
