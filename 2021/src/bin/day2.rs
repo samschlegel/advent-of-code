2b0b51edd4215f688b0a4b097985fb30abb80210
Sam Schlegel <git@lutin.us>
2021-12-02 20:03:48 -0800

    jank part b

--- 2021/src/bin/day2.rs
+++ 2021/src/bin/day2.rs
@@ -2,14 +2,15 @@ use std::io;
 
 #[derive(Debug, Default, Clone, Copy)]
 struct Position {
-    horizontal: f32,
-    depth: f32,
+    aim: i64,
+    horizontal: i64,
+    depth: i64,
 }
 
 fn part_a<I: Iterator<Item = S>, S: AsRef<str>>(lines: I) -> impl Iterator<Item = Position> {
     lines.scan(Position::default(), |state, line| {
         let (command, x_str) = line.as_ref().split_once(' ').unwrap();
-        let x: f32 = x_str.parse().unwrap();
+        let x: i64 = x_str.parse().unwrap();
         match command {
             "forward" => state.horizontal += x,
             "down" => state.depth += x,
@@ -20,12 +21,36 @@ fn part_a<I: Iterator<Item = S>, S: AsRef<str>>(lines: I) -> impl Iterator<Item 
     })
 }
 
+fn part_b<I: Iterator<Item = S>, S: AsRef<str>>(lines: I) -> impl Iterator<Item = Position> {
+    lines.scan(Position::default(), |state, line| {
+        let (command, x_str) = line.as_ref().split_once(' ').unwrap();
+        let x: i64 = x_str.parse().unwrap();
+        match command {
+            "forward" => {
+                state.horizontal += x;
+                state.depth += state.aim * x;
+            }
+            "down" => state.aim += x,
+            "up" => state.aim -= x,
+            _ => {}
+        }
+        Some(state.clone())
+    })
+}
+
+const TEST_INPUT: &str = include_str!("../../inputs/day2_test.txt");
+
 const INPUT: &str = include_str!("../../inputs/day2.txt");
 
 fn main() -> io::Result<()> {
     let final_pos = part_a(INPUT.lines()).last().unwrap();
     println!("{:?}", final_pos);
     println!("Multiplied: {}", final_pos.horizontal * final_pos.depth);
+
+    let final_pos = part_b(INPUT.lines()).last().unwrap();
+    println!("{:?}", final_pos);
+    println!("Multiplied: {}", final_pos.horizontal * final_pos.depth);
+
     Ok(())
 }
 
