cb3ca460613ce1b4a0e8327244e8ba2827507375
Sam Schlegel <Sam Schlegel>
2025-12-08 18:09:10 -0800

    d7p2 type crimes

--- 2025/day7/main.py
+++ 2025/day7/main.py
@@ -39,7 +39,36 @@ def part1(filename):
 
 
 def part2(filename):
-    pass
+    with open(filename, "r") as f:
+        lines = [list(l.strip()) for l in f]
+    cols = len(lines[0])
+    splits = 0
+    for i in range(1, len(lines)):
+        prev = lines[i - 1]
+        curr = lines[i]
+        for j in range(0, cols):
+            if curr[j] == "." or isinstance(curr[j], int):
+                if prev[j] == "S":
+                    curr[j] = 1
+                    continue
+                if isinstance(prev[j], int):
+                    try:
+                        curr[j] += prev[j]
+                    except TypeError:
+                        curr[j] = prev[j]
+            elif curr[j] == "^":
+                if isinstance(prev[j], int):
+                    try:
+                        curr[j - 1] += prev[j]
+                    except TypeError:
+                        curr[j - 1] = prev[j]
+                    try:
+                        curr[j + 1] += prev[j]
+                    except TypeError:
+                        curr[j + 1] = prev[j]
+    for line in lines:
+        print("".join(str(x) for x in line))
+    print(sum(x for x in lines[-1] if isinstance(x, int)))
 
 
 if __name__ == "__main__":
