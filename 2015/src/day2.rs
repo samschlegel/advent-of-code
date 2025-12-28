6506ca4e027779d890dbef1f76d00eb07e9b3ddf
Sam Schlegel <git@lutin.us>
2025-12-27 18:14:23 -0800

    d2

---
+++ 2015/src/day2.rs
@@ -0,0 +1,69 @@
+use std::num::ParseIntError;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+use nom::Parser;
+use nom::bytes::tag;
+use nom::character::digit1;
+use nom::combinator::map_res;
+use nom::error::ErrorKind;
+use nom::multi::separated_list1;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Gift {
+    pub length: usize,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl Gift {
+    pub fn surface_area(&self) -> usize {
+        let lw = self.length * self.width;
+        let wh = self.width * self.height;
+        let hl = self.height * self.length;
+        2 * (lw + wh + hl)
+    }
+
+    pub fn slack_area(&self) -> usize {
+        let lw = self.length * self.width;
+        let wh = self.width * self.height;
+        let hl = self.height * self.length;
+        *[lw, wh, hl].iter().min().unwrap()
+    }
+}
+
+#[aoc_generator(day2)]
+fn input_generator(input: &str) -> Vec<Gift> {
+    let (input, gifts) = separated_list1(
+        tag("\n"),
+        map_res(
+            (
+                digit1::<_, (_, ErrorKind)>(),
+                tag("x"),
+                digit1::<_, (_, ErrorKind)>(),
+                tag("x"),
+                digit1::<_, (_, ErrorKind)>(),
+            ),
+            |(l, _, w, _, h): (&str, &str, &str, &str, &str)| {
+                Ok::<Gift, ParseIntError>(Gift {
+                    length: l.parse::<usize>()?,
+                    width: w.parse::<usize>()?,
+                    height: h.parse::<usize>()?,
+                })
+            },
+        ),
+    )
+    .parse_complete(&input)
+    .unwrap();
+
+    assert_eq!(input, "");
+
+    gifts
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &Vec<Gift>) -> usize {
+    input
+        .iter()
+        .map(|g| g.surface_area() + g.slack_area())
+        .sum()
+}
